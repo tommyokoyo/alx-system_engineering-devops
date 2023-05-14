63c7841cc5c9aaeea599e4771c6dad7a9a68a26f
tommyokoyo <okoyotommy@gmail.com>
2023-05-14 14:16:43 -0400

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+This scripts fetches Number of subscribers for a given subreddit
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    queries for number of subscribers
+    """
+    req_headers = {"user-agent":" Apple-safari"}
+    base_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    try:
+        data = requests.get(base_url, headers = req_headers).json()
+        number_of_subscribers = data["data"]["subscribers"]
+        return number_of_subscribers
+
+    except Exception:
+        return 0
