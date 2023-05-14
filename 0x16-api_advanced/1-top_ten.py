ee341761c7f7f32dc4539401d9200aa5b7576d7a
tommyokoyo <okoyotommy@gmail.com>
2023-05-14 15:14:40 -0400

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,6 +5,7 @@ Queries for titles of the first 10 hotspots
 
 import requests
 
+
 def top_ten(subreddit):
     base_url = "https://api.reddit.com/r/{}/hot?limit=10".format(subreddit)
     req_headers = {"user-agent": " Apple-safari"}
