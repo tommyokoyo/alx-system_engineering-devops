5bd9158112b788f23d797f237bc6d79dab78be14
tommyokoyo <okoyotommy@gmail.com>
2023-05-14 15:31:26 -0400

    Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """
-recursive function the queries reddit api and returns a list containing titles of hot articles for a subreddit
+recursive function the queries reddit api and
+returns a list containing titles of hot articles for a subreddit
 """
 import requests
 
 
 def recurse(subreddit, hot_list=[], next=""):
-    base_url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, next)
+    base_url = "https://api.reddit.com/r/{}/hot?after={}".format(
+        subreddit, next)
     req_headers = {"user-agent": " Apple-safari"}
     try:
-        data = requests.get(base_url, headers=req_headers, allow_redirects=False).json()
+        data = requests.get(base_url, headers=req_headers,
+                            allow_redirects=False).json()
         hot_posts = data['data']['children']
         for posts in hot_posts:
             hot_list.append(posts['data']['title'])
