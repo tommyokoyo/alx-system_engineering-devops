14789be5b054faae9bd39f15cdb9449708a5e900
tommyokoyo <okoyotommy@gmail.com>
2023-05-14 14:48:41 -0400

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,15 +5,16 @@ This scripts fetches Number of subscribers for a given subreddit
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
     queries for number of subscribers
     """
-    req_headers = {"user-agent":" Apple-safari"}
+    req_headers = {"user-agent": " Apple-safari"}
     base_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     try:
-        data = requests.get(base_url, headers = req_headers).json()
+        data = requests.get(base_url, headers=req_headers).json()
         number_of_subscribers = data["data"]["subscribers"]
         return number_of_subscribers
 
